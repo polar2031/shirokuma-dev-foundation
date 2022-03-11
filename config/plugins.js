f9b2e7d91707baf566f8dd355b1e783261ae6b64
Kuan-Lun Chen <polar2031@gmail.com>
2022-03-11 12:19:55 +0800

    adjust react image size

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            breakpoints: {
+                large: 2000,
+                medium: 1000,
+                small: 500,
+            }
+        }
+    }
+})
