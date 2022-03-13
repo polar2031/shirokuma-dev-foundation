483ece33c3f69b2b03289f770164cb19ea27fffc
Kuan-Lun Chen <polar2031@gmail.com>
2022-03-13 21:47:37 +0800

    read salt from env

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '3005af5f5dc2711f27335f073d048e3b'),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '074bd92c088206545499020ed896ec4c'),
+    secret: env('ADMIN_JWT_SECRET', 'e682d4db280187272f2a51d7c340abe6'),
   },
 });
