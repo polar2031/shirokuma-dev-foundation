4d536c40d2b61ef66907990b4633abc12b49850c
Kuan-Lun Chen <polar2031@gmail.com>
2022-03-24 01:47:13 +0800

    fix: article lifecycle trigger issue  canonicalUrl update won't trigger change on removed one

--- src/api/article/content-types/article/lifecycles.js
+++ src/api/article/content-types/article/lifecycles.js
@@ -1,34 +1,73 @@
 // send revalidate request to next blog
-async function revalidate(canonicalUrl) {
+async function revalidate(canonicalUrl, purge = false) {
+  let retry = purge ? 5 : 1;
+  const axios = require("axios");
   if (process.env.BLOG_REVALIDATE) {
-    const axios = require("axios");
-    await axios
-      .get(process.env.BLOG_REVALIDATE_ARTICLE_URL, {
-        params: {
-          secret: process.env.BLOG_REVALIDATE_TOKEN,
-          canonical: canonicalUrl,
-        },
-      })
-      .then((_res) => {
-        strapi.log.info(
-          `update ${canonicalUrl} successfully with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
-        );
-      })
-      .catch((_error) => {
-        strapi.log.error(
-          `fail to update ${canonicalUrl} with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
-        );
-      });
+
+    while (retry > 0) {
+      if (purge) {
+        // wait 10 sec to make sure update is done
+        await new Promise((r) => setTimeout(r, 1000));
+      }
+      await axios
+        .get(process.env.BLOG_REVALIDATE_ARTICLE_URL, {
+          params: {
+            secret: process.env.BLOG_REVALIDATE_TOKEN,
+            canonical: canonicalUrl,
+          },
+        })
+        .then((_res) => {
+          if (!purge) {
+            strapi.log.info(
+              `update ${canonicalUrl} successfully with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
+            );
+          } else {
+            if (retry > 1) {
+              strapi.log.info(
+                `fail to purge ${canonicalUrl} successfully with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}, will retry in 1 sec`
+              );
+            } else {
+              strapi.log.error(
+                `fail to purge ${canonicalUrl} successfully with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
+              );
+            }
+          }
+        })
+        .catch((_error) => {
+          if (!purge) {
+            strapi.log.error(
+              `fail to update ${canonicalUrl} with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
+            );
+          } else {
+            strapi.log.info(
+              `purge ${canonicalUrl} successfully with ${process.env.BLOG_REVALIDATE_ARTICLE_URL}`
+            );
+            retry = 0;
+          }
+        });
+      retry -= 1;
+    }
   }
 }
 
 module.exports = {
+  async beforeUpdate(event) {
+    const entry = await strapi.db.query("api::article.article").findOne({
+      select: ["canonicalUrl"],
+      where: { ...event.params.where },
+    });
+    if (event.params.data.canonicalUrl != entry.canonicalUrl) {
+      // we would like to let afterUpdate done first
+      // so no await here
+      revalidate(entry.canonicalUrl, true);
+    }
+  },
   async afterUpdate(event) {
     const entry = await strapi.db.query("api::article.article").findOne({
       select: ["canonicalUrl", "publishedAt"],
       where: { ...event.params.where },
     });
-    // only
+    // only published article or change on published status should trigger this
     if ("publishedAt" in event.params.data || entry.publishedAt) {
       await revalidate(entry.canonicalUrl);
     }
