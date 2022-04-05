45277cf7c2e6bb53e56d81003d4f53c9b21920a8
Kuan-Lun Chen <polar2031@gmail.com>
2022-04-06 00:00:54 +0800

    package and config update

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
   apiToken: {
-    salt: env('API_TOKEN_SALT', '3005af5f5dc2711f27335f073d048e3b'),
+    salt: env("API_TOKEN_SALT", "3005af5f5dc2711f27335f073d048e3b"),
   },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e682d4db280187272f2a51d7c340abe6'),
+    secret: env("ADMIN_JWT_SECRET", "e682d4db280187272f2a51d7c340abe6"),
   },
+  url: "geass",
 });
